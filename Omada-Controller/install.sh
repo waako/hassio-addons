62710022637b656b1d3bb65e4953e729c45c9941
leadingbump <34223396+leadingbump@users.noreply.github.com>
2022-06-20 13:44:42 -0600

    Update install.sh

--- Omada-Controller/install.sh
+++ Omada-Controller/install.sh
@@ -2,39 +2,81 @@
 
 set -e
 
-OMADA_DIR="/data/omada_controller"
+# omada controller dependency and package installer script for versions 4.x and 5.x
+
+# set default variables
+OMADA_DIR="/opt/tplink/EAPController"
 ARCH="${ARCH:-}"
 OMADA_VER="${OMADA_VER:-}"
 OMADA_TAR="${OMADA_TAR:-}"
 OMADA_URL="${OMADA_URL:-}"
+OMADA_MAJOR_VER="${OMADA_VER%.*.*}"
+OMADA_MAJOR_MINOR_VER="${OMADA_VER%.*}"
 
 die() { echo -e "$@" 2>&1; exit 1; }
 
+
+echo "**** Selecting packages based on the architecture and version ****"
 # common package dependencies
 PKGS=(
   gosu
   net-tools
-  openjdk-8-jre-headless
   tzdata
   wget
 )
 
+# add specific package for mongodb
 case "${ARCH}" in
-amd64|arm64|"")
-  PKGS+=( mongodb-server-core )
-  ;;
-armv7l)
-  PKGS+=( mongodb )
-  ;;
-*)
-  die "${ARCH}: unsupported ARCH"
-  ;;
+  amd64|arm64|"")
+    PKGS+=( mongodb-server-core )
+    ;;
+  armv7l)
+    PKGS+=( mongodb )
+    ;;
+  *)
+    die "${ARCH}: unsupported ARCH"
+    ;;
 esac
 
+# add specific package for openjdk
+case "${ARCH}" in
+  amd64|arm64|"")
+    # use openjdk-17 for v5.4 and above; all others us openjdk-8
+    case "${OMADA_MAJOR_VER}" in
+      5)
+        # pick specific package based on the major.minor version
+        case "${OMADA_MAJOR_MINOR_VER}" in
+          5.0|5.1|5.3)
+            # 5.0 to 5.3 all use openjdk-8
+            PKGS+=( openjdk-8-jre-headless )
+            ;;
+          *)
+            # starting with 5.4, openjdk-17 is supported
+            PKGS+=( openjdk-17-jre-headless )
+            ;;
+        esac
+        ;;
+      *)
+        # all other versions, use openjdk-8
+        PKGS+=( openjdk-8-jre-headless )
+        ;;
+    esac
+    ;;
+  armv7l)
+    # always use openjdk-8 for armv7l
+    PKGS+=( openjdk-8-jre-headless )
+    ;;
+  *)
+    die "${ARCH}: unsupported ARCH"
+    ;;
+esac
+
+# output variables/selections
 echo "ARCH=${ARCH}"
 echo "OMADA_VER=${OMADA_VER}"
 echo "OMADA_TAR=${OMADA_TAR}"
 echo "OMADA_URL=${OMADA_URL}"
+echo "PKGS=( ${PKGS[*]} )"
 
 echo "**** Install Dependencies ****"
 export DEBIAN_FRONTEND=noninteractive
@@ -46,18 +88,53 @@ cd /tmp
 wget -nv "${OMADA_URL}"
 
 echo "**** Extract and Install Omada Controller ****"
-tar zxvf "${OMADA_TAR}"
-rm -f "${OMADA_TAR}"
-cd Omada_SDN_Controller_*
+
+# in the 4.4.3, 4.4.6, and 4.4.8 builds, they removed the directory. this case statement will handle variations in the build
+case "${OMADA_VER}" in
+  4.4.3|4.4.6|4.4.8)
+    echo "version ${OMADA_VER}"
+    mkdir "Omada_SDN_Controller_${OMADA_VER}"
+    cd "Omada_SDN_Controller_${OMADA_VER}"
+    tar zxvf "../${OMADA_TAR}"
+    rm -f "../${OMADA_TAR}"
+    ;;
+  *)
+    echo "not version 4.4.3/4.4.6/4.4.8"
+    tar zxvf "${OMADA_TAR}"
+    rm -f "${OMADA_TAR}"
+    cd Omada_SDN_Controller_*
+    ;;
+esac
+
+# make sure tha the install directory exists
 mkdir "${OMADA_DIR}" -vp
-cp bin "${OMADA_DIR}" -r
-cp data "${OMADA_DIR}" -r
-cp properties "${OMADA_DIR}" -r
-cp webapps "${OMADA_DIR}" -r
-cp keystore "${OMADA_DIR}" -r
-cp lib "${OMADA_DIR}" -r
-cp install.sh "${OMADA_DIR}" -r
-cp uninstall.sh "${OMADA_DIR}" -r
+
+# starting with 5.0.x, the installation has no webapps directory; these values are pulled from the install.sh
+case "${OMADA_MAJOR_VER}" in
+  5)
+    # check which 5.x we are running
+    case "${OMADA_VER}" in
+      5.3.1)
+        # 5.3.1 move the keystore directory to be a subdir of data
+        NAMES=( bin data properties lib install.sh uninstall.sh )
+        ;;
+      *)
+        NAMES=( bin data properties keystore lib install.sh uninstall.sh )
+        ;;
+    esac
+    ;;
+  *)
+    NAMES=( bin data properties keystore lib webapps install.sh uninstall.sh )
+    ;;
+esac
+
+# copy over the files to the destination
+for NAME in "${NAMES[@]}"
+do
+  cp "${NAME}" "${OMADA_DIR}" -r
+done
+
+# symlink for mongod
 ln -sf "$(which mongod)" "${OMADA_DIR}/bin/mongod"
 chmod 755 "${OMADA_DIR}"/bin/*
 
